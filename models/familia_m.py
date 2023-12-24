e7748b8d92ac31f20beab114996d7f4ac8e48740
YensiMejiaPescador <ymsoftsas@gmail.com>
2023-12-23 20:58:32 -0500

    add: modifica vista

--- models/familia_m.py
+++ models/familia_m.py
@@ -3,6 +3,19 @@ from odoo import api, fields, models
 
 class FamiliaM(models.Model):
     _inherit = 'familia.m'
-    my_nickname = fields.Char(string="My Nickname")
-    my_age = fields.Integer(string="My Age")
-    main_user = fields.Boolean(string="Is main user?")
+    lead_user = fields.Boolean(string="Lead User",
+                               compute="_compute_lead_user")
+    not_lead_user = fields.Boolean(string="Not Lead User",
+                                   compute="_compute_not_lead_user",
+                                   store=True)
+    my_db = fields.char(default=lambda self: self.env.cr.dbname, string="Data Base")
+
+    @api.depends("main_user")
+    def _compute_lead_user(self):
+        for record in self:
+            record.lead_user = record.main_user
+
+    @api.depends("main_user")
+    def _compute_not_lead_user(self):
+        for record in self:
+            record.not_lead_user = not record.main_user
